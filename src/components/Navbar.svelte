eb7598788b237608236ab79b4f446b2ffb77c268
ch-iv <icheppy101@gmail.com>
2022-09-16 17:06:56 -0400

    remove spinning logo animation

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,6 +1,6 @@
 <div class="flex justify-between my-3 items-center md:flex-row lg:flex-row flex-col">
 	<div class="flex gap-3 items-center">
-		<img src="gear_logo_colored.png" alt="logo" class="h-14 w-14 spinning-logo" />
+		<img src="gear_logo_colored.png" alt="logo" class="h-14 w-14" />
 		<div class="text-2xl font-bold">Woodlands Tech Club</div>
 	</div>
 	<h5>📍 Mississauga, ON</h5>
