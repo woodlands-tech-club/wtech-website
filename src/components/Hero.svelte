fe2e10bb782d5ecca6cd4de1fd2c22cd49fa9ca0
ch-iv <icheppy101@gmail.com>
2022-09-16 19:33:42 -0400

    add social media icons

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,6 +1,11 @@
+<script>
+	export let discord_link;
+	export let instagram_link;
+</script>
+
 <div class="flex flex-col md:flex-row items-center gap-5 my-10">
 	<div class="md:w-[50%]">
-		<p class="font-semibold mb-2 text-xl text-blue-400">
+		<p class="font-semibold mb-2 text-xl">
 			The Woodlands Tech Club is a school club of the Woodlands Public School in Mississauga, ON.
 		</p>
 
@@ -8,6 +13,15 @@
 			We are a group of students who are passionate about technological education. We strive to
 			promote tech literacy and make it accessible to everyone.
 		</p>
+
+		<div class="mt-6 flex gap-5">
+			<a href={discord_link}>
+				<img src="/discord.svg" alt="discord" class="h-9 blue-500" />
+			</a>
+			<a href={instagram_link}>
+				<img src="/instagram.svg" alt="discord" class="h-9 blue-500" />
+			</a>
+		</div>
 	</div>
 	<img src="students.jpg" alt="group photo" class="md:w-[50%] md:h-auto drop-shadow" />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,12 @@
 	import WhatWeDo from '../components/WhatWeDo.svelte';
 	import Blog from '../components/Blog.svelte';
 	import Team from '../components/Team.svelte';
+
+	let discord_link = 'https://discord.gg/3s4DYcbJ37';
+	let instagram_link = 'https://www.instagram.com/woodlands.tech/';
 </script>
 
-<Hero />
+<Hero {discord_link} {instagram_link} />
 <Blog />
 <WhatWeDo />
 <Team />
