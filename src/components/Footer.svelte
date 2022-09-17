916c2b527a48e9dbad83e23e3b9b198cec028de3
ch-iv <icheppy101@gmail.com>
2022-09-16 20:42:45 -0400

    add footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,15 @@
+<hr class="my-6" />
+<div class="flex justify-between items-center md:flex-row lg:flex-row flex-col mb-2">
+	<a href="/">
+		<div class="flex gap-3 items-center">
+			<div class="text-2xl font-semibold">Woodlands Tech Club Website</div>
+		</div>
+	</a>
+</div>
+<div class="mb-3">
+	Made by <a href="https://github.com/ch-iv" class="text-blue-500">Ivan Chepelev</a> using
+	<a href="https://kit.svelte.dev/" class="text-blue-500">SvelteKit</a>
+	and <a href="https://tailwindcss.com/" class="text-blue-500">Tailwind CSS</a>. Website source code
+	can be found
+	<a href="https://github.com/woodlands-tech-club/wtech-website" class="text-blue-500">here</a>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script>
 	import '../app.css';
 	import Navbar from '../components/Navbar.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
 
 <main class="lg:mx-[20%] mx-[10%]">
 	<Navbar />
 
 	<slot />
+
+	<Footer />
 </main>
